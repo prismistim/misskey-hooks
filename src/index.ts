d2c0fdcd6fc08316a64a2292da5eb23218a75921
snowsphere <info@snowsphere.net>
2025-07-09 23:22:23 +0900

    できた

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,47 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+		if (request.method !== 'POST') {
+			return new Response('Method Not Allowed', { status: 405 })
+		}
+
+		const url = new URL(request.url)
+
+		if (!url.pathname.endsWith('/notify')) {
+			return new Response('Not Found', { status: 404 })
+		}
+
+		if (request.headers.get('X-Misskey-Hook-Secret') !== env.MISSKEY_HOOK_SECRET) {
+			return new Response('Unauthorized', { status: 401 })
+		}
+
+		const requestBody = await request.text()
+
+		if (!requestBody) {
+			return new Response('Bad Request', { status: 400 })
+		}
+
+		const body = JSON.parse(requestBody)
+
+		try {
+			const response = await fetch(env.DISCORD_HOOK_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					content: `**${body.type}**\n\n\`\`\`json\n)${JSON.stringify(body, null, 2)}\n\`\`\``
+				})
+			})
+
+			if (!response.ok) {
+				return new Response('Failed to send message', { status: 500 })
+			}
+		} catch (e) {
+			console.error('Error sending message to Discord:', e)
+			return new Response('Internal Server Error', { status: 500 })
+		}
+
+		return new Response('ok')
+	}
+} satisfies ExportedHandler<Env>
