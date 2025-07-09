5d8869621c76c52863f7bf56818ae8a4f61e3d79
snowsphere <info@snowsphere.net>
2025-07-09 23:39:46 +0900

    tabをspaceに変えた

--- src/index.ts
+++ src/index.ts
@@ -12,48 +12,54 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		if (request.method !== 'POST') {
-			return new Response('Method Not Allowed', { status: 405 })
-		}
+  async fetch(request, env, ctx): Promise<Response> {
+    if (request.method !== "POST") {
+      return new Response("Method Not Allowed", { status: 405 });
+    }
 
-		const url = new URL(request.url)
+    const url = new URL(request.url);
 
-		if (!url.pathname.endsWith('/notify')) {
-			return new Response('Not Found', { status: 404 })
-		}
+    if (!url.pathname.endsWith("/notify")) {
+      return new Response("Not Found", { status: 404 });
+    }
 
-		if (request.headers.get('X-Misskey-Hook-Secret') !== env.MISSKEY_HOOK_SECRET) {
-			return new Response('Unauthorized', { status: 401 })
-		}
+    if (
+      request.headers.get("X-Misskey-Hook-Secret") !== env.MISSKEY_HOOK_SECRET
+    ) {
+      return new Response("Unauthorized", { status: 401 });
+    }
 
-		const requestBody = await request.text()
+    const requestBody = await request.text();
 
-		if (!requestBody) {
-			return new Response('Bad Request', { status: 400 })
-		}
+    if (!requestBody) {
+      return new Response("Bad Request", { status: 400 });
+    }
 
-		const body = JSON.parse(requestBody)
+    const body = JSON.parse(requestBody);
 
-		try {
-			const response = await fetch(env.DISCORD_HOOK_URL, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					content: `**${body.type}**\n\n\`\`\`json\n)${JSON.stringify(body, null, 2)}\n\`\`\``
-				})
-			})
+    try {
+      const response = await fetch(env.DISCORD_HOOK_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: `**${body.type}**\n\n\`\`\`json\n)${JSON.stringify(
+            body,
+            null,
+            2
+          )}\n\`\`\``,
+        }),
+      });
 
-			if (!response.ok) {
-				return new Response('Failed to send message', { status: 500 })
-			}
-		} catch (e) {
-			console.error('Error sending message to Discord:', e)
-			return new Response('Internal Server Error', { status: 500 })
-		}
+      if (!response.ok) {
+        return new Response("Failed to send message", { status: 500 });
+      }
+    } catch (e) {
+      console.error("Error sending message to Discord:", e);
+      return new Response("Internal Server Error", { status: 500 });
+    }
 
-		return new Response('ok')
-	}
-} satisfies ExportedHandler<Env>
+    return new Response("ok");
+  },
+} satisfies ExportedHandler<Env>;
